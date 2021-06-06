d8361473be418f6a0272cdcb611c2a7a8a0b0eb4
Matthew Hawkins <matthew.hawkins1115@gmail.com>
2021-06-06 00:15:00 -0700

    Multiply not working on purpose

--- test_calculator.py
+++ test_calculator.py
@@ -6,3 +6,5 @@ class TestCalculator:
         assert 6 == calculator.add(1,5)
     def test_subtract(self):
         assert 2 == calculator.subtract(4,2)
+    def test_multply(self):
+        assert 4 == calculator.multiply(4,1)
